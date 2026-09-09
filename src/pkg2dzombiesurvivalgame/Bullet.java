e44e97dc46decc258f388851c36e86389e800448
Arnab-Day <arnab8198@gmail.com>
2026-09-09 14:00:00 +0600

    Week 7 - Shooting and Collision Detection

--- src/pkg2dzombiesurvivalgame/Bullet.java
+++ src/pkg2dzombiesurvivalgame/Bullet.java
@@ -1,13 +1,13 @@
 package pkg2dzombiesurvivalgame;
 
-public class Bullet {
-    int x;
-    int y;
-    int speed;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
-    public Bullet(int x, int y) {
-        this.x = x;
-        this.y = y;
-        speed = 8;
-    }
+public class Bullet {
+    int x,y; int speed=8;
+    public Bullet(int x,y){this.x=x;this.y=y;}
+    public void move(){x+=speed;}
+    public void draw(Graphics g){g.setColor(Color.YELLOW);g.fillRect(x,y,10,5);}
+    public Rectangle getBounds(){return new Rectangle(x,y,10,5);}
 }
--- src/pkg2dzombiesurvivalgame/GamePanel.java
+++ src/pkg2dzombiesurvivalgame/GamePanel.java
@@ -14,54 +14,53 @@ public class GamePanel extends JPanel implements KeyListener {
     final int screenWidth=800, screenHeight=600;
     Player player;
     ArrayList<Zombie> zombies=new ArrayList<>();
+    ArrayList<Bullet> bullets=new ArrayList<>();
     boolean up,down,left,right;
+    int score=0;
     Random random=new Random();
 
     public GamePanel(){
         setPreferredSize(new Dimension(screenWidth,screenHeight));
-        setBackground(Color.BLACK);
-        setDoubleBuffered(true);
-        setFocusable(true);
-        player=new Player();
-        addKeyListener(this);
-        requestFocusInWindow();
+        setBackground(Color.BLACK); setFocusable(true); player=new Player();
+        addKeyListener(this); requestFocusInWindow();
         for(int i=0;i<5;i++){
-            int x=random.nextInt(screenWidth-40);
-            int y=random.nextInt(screenHeight-40);
+            int x=random.nextInt(screenWidth-40); int y=random.nextInt(screenHeight-40);
             zombies.add(new Zombie(x,y));
         }
-        Timer timer=new Timer(16,e->updateGame());
-        timer.start();
+        Timer timer=new Timer(16,e->updateGame()); timer.start();
     }
-
     private void updateGame(){
         player.move(up,down,left,right);
         for(Zombie zombie:zombies) zombie.moveToward(player);
-        repaint();
+        for(Bullet bullet:bullets) bullet.move();
+        for(int i=bullets.size()-1;i>=0;i--){
+            Bullet bullet=bullets.get(i);
+            for(int j=zombies.size()-1;j>=0;j--){
+                Zombie zombie=zombies.get(j);
+                if(bullet.getBounds().intersects(zombie.getBounds())){
+                    bullets.remove(i); zombies.remove(j); score++; break;
+                }
+            }
+        }
+        bullets.removeIf(b->b.x>screenWidth); repaint();
     }
-
     @Override protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.setColor(Color.WHITE);
-        g.drawString("Zombie Survival Game",330,30);
-        player.draw(g);
-        for(Zombie zombie:zombies) zombie.draw(g);
+        g.setColor(Color.WHITE); g.drawString("Zombie Survival Game",330,30);
+        g.drawString("Score: "+score,20,30);
+        player.draw(g); for(Zombie zombie:zombies) zombie.draw(g); for(Bullet bullet:bullets) bullet.draw(g);
     }
-
     @Override public void keyPressed(KeyEvent e){
         switch(e.getKeyCode()){
-            case KeyEvent.VK_W: up=true; break;
-            case KeyEvent.VK_S: down=true; break;
-            case KeyEvent.VK_A: left=true; break;
-            case KeyEvent.VK_D: right=true; break;
+            case KeyEvent.VK_W: up=true; break; case KeyEvent.VK_S: down=true; break;
+            case KeyEvent.VK_A: left=true; break; case KeyEvent.VK_D: right=true; break;
+            case KeyEvent.VK_SPACE: bullets.add(new Bullet(player.x+player.width,player.y+player.height/2)); break;
         }
     }
     @Override public void keyReleased(KeyEvent e){
         switch(e.getKeyCode()){
-            case KeyEvent.VK_W: up=false; break;
-            case KeyEvent.VK_S: down=false; break;
-            case KeyEvent.VK_A: left=false; break;
-            case KeyEvent.VK_D: right=false; break;
+            case KeyEvent.VK_W: up=false; break; case KeyEvent.VK_S: down=false; break;
+            case KeyEvent.VK_A: left=false; break; case KeyEvent.VK_D: right=false; break;
         }
     }
     @Override public void keyTyped(KeyEvent e){}
